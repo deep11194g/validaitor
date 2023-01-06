de36beaba8a04761a131474366d57971c53c2c94
deep11194g <soumyadeep11194@gmail.com>
2023-01-06 18:53:14 +0530

    Trying to add download report functionality

--- pred_models/urls.py
+++ pred_models/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.pred_models_list, name='pred_models_list'),
     path('<int:pred_model_id>', views.pred_model_detail, name='pred_model_detail'),
     path('upload', views.pred_model_upload, name='pred_model_upload'),
-    path('generate_report/<int:pred_model_id>', views.generate_report, name='generate_report')
+    path('<int:pred_model_id>/generate_report', views.generate_report, name='generate_report'),
+    path('<int:pred_model_id>/download_report', views.generate_report, name='download_report')
 ]
--- pred_models/views.py
+++ pred_models/views.py
@@ -1,11 +1,15 @@
 import traceback
 
+from fpdf import FPDF
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render
+from django.conf import settings
 
 from pred_models.forms import PredModelForm
-from pred_models.models import PredModel
+from pred_models.models import PredModel, get_folder_path
 from pred_models.analysis import PerformanceStatsAnalyser
+
+from django.http import HttpResponse
 
 
 @login_required(login_url='login')
@@ -83,7 +87,7 @@ def generate_report(request, pred_model_id):
         print(traceback.format_exc())
         message = {
             'color': 'red',
-            'text':'Error in model test metric generation'
+            'text': 'Error in model test metric generation'
         }
     return render(
         request, 'pred_models/view.html',
@@ -93,3 +97,46 @@ def generate_report(request, pred_model_id):
             'message': message
         }
     )
+
+
+@login_required(login_url='login')
+def download_report(request, pred_model_id):
+    # TODO: Not working yet
+    pred_model = PredModel.objects.get(pk=pred_model_id)
+    message = None
+    if pred_model.developer != request.user:
+        pred_model = None
+
+    if not pred_model:
+        message = {
+            'color': 'red',
+            'text': 'Pred model with ID {} doesnt exists or belong to you'.format(pred_model_id)
+        }
+    if not (pred_model.confusion_matrix or pred_model.classification_report or pred_model.roc_plot
+            or pred_model.prc_plot):
+        message = {
+            'color': 'red',
+            'text': 'Report for Pred model with ID {} has not been generated yet'.format(pred_model_id)
+        }
+    if message:
+        return render(
+            request, 'pred_models/view.html',
+            {
+                'pred_model': pred_model,
+                'pred_model_id': pred_model_id,
+                'message': message
+            }
+        )
+
+    # Generate PDF and store in the directory as other model files
+    pdf = FPDF()
+    for image_file in [pred_model.confusion_matrix.path, pred_model.classification_report.partition(),
+                       pred_model.roc_plot.path, pred_model.prc_plot.path]:
+        pdf.add_page()
+        pdf.image(image_file)
+    pdf_store_path = settings.MEDIA_ROOT + '/'+ get_folder_path(pred_model, 'performance_report.pdf')
+    pdf.output(pdf_store_path, "F")
+
+    response = HttpResponse(pdf, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="{}"'.format(pdf_store_path)
+    return response
